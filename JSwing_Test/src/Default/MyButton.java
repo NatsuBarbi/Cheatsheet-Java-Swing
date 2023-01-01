76d2f52abfaf27c0782a53f139c5382d74a4089f
Barbi <93249044+NatsuBarbi@users.noreply.github.com>
2023-01-01 17:59:59 +0100

    Update MyButton.java

--- JSwing_Test/src/Default/MyButton.java
+++ JSwing_Test/src/Default/MyButton.java
@@ -7,13 +7,13 @@ import java.awt.event.ActionListener;
 
 public class MyButton extends JButton {
     MyButton(String text){
-        this.setBounds(200, 100, 200, 50);      // set Button Size
-        this.setText(text);                                     // set Button Text
+        this.setBounds(200, 100, 200, 50);                              // set Button Size
+        this.setText(text);                                             // set Button Text
         this.setHorizontalTextPosition(JButton.CENTER);
         this.setVerticalAlignment(JButton.CENTER);
-        this.setFont(new Font("Consolas", Font.BOLD, 25));  // Text Formats
-        this.setForeground(Color.red);                               // Text Color
-        this.setBackground(Color.cyan);                              // Button Background
+        this.setFont(new Font("Consolas", Font.BOLD, 25));              // Text Formats
+        this.setForeground(Color.red);                                  // Text Color
+        this.setBackground(Color.cyan);                                 // Button Background
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3)); // set Border
     }
 
